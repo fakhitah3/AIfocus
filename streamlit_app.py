9b0e006a862e581d1a5b6deefd7e0690f96b00bb
fakhitah3 <fakhitah.ridzuan@gmail.com>
2024-10-09 16:19:10 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,19 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+# Page titles for navigation
+PAGES = {
+    "Home": "streamlit_app.py",
+    "Selangor": "pages/Selangor.py",
+}
+
+# Sidebar for navigation
+st.sidebar.title("Navigation")
+selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+
+# Load the selected page
+if selection == "Home":
+    import page1
+elif selection == "Data Analysis":
+    import page2
